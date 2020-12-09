a27e53c15736fc83387f9bd770ebd20abad3035e
Christopher Hural Yogodzinski <cyogodzi@c6145-2-8.local>
2020-12-09 17:08:13 -0500

    changed how output is named

--- filter-alt-mouse-contigs.sh
+++ filter-alt-mouse-contigs.sh
@@ -19,7 +19,7 @@ for var in "$@";
 
 		bcftools view $zipped \
 		--regions chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chrX,chrY,chrM \
-		> "filtered-"$var
+		> $var".filtered.vcf"
 
 		echo "Done!"
 	done;
