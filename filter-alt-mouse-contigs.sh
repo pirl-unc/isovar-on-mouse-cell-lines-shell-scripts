e1fad0c013de9f11be5d0fbd7635c720e253c216
Christopher Hural Yogodzinski <cyogodzi@c6145-2-8.local>
2020-12-09 17:02:45 -0500

    alternate mouse contig filter script

---
+++ filter-alt-mouse-contigs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for var in "$@";
+	do 
+		if [[ $var == *.vcf ]]; then 
+        		echo "Compressing $var for bcftools"
+			gzip $var 
+			zcat $var | bgzip -c > "new."$var".gz" && tabix "new."$var".gz"
+			zipped="new."$var".gz"
+			
+
+		elif [[ $var == *.gz ]]; then
+			echo 
+			zcat $var | bgzip -c > "new."$var && tabix "new."$var	
+			zipped="new."$var
+	   	fi
+
+		echo "Filtering Alt Mouse Contigs in" $var
+
+		bcftools view $zipped \
+		--regions chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chrX,chrY,chrM \
+		> "filtered-"$var
+
+		echo "Done!"
+	done;
